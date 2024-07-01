1c25f4c8f1d78c35a6b16a7a5428cb12a29ecf8c
HassanMahmoodAwan <hma.hassan.2001@gmail.com>
2024-07-01 16:46:59 +0500

    .env file removed

--- Backend/Routes/unStitchedRoutes.js
+++ Backend/Routes/unStitchedRoutes.js
@@ -5,7 +5,7 @@ const { allUnstitchedCloths, getByIdUnstitchedCloth, printedWear, embroderedWear
 const route = express.Router()
 
 route.route('/').get(allUnstitchedCloths)
-route.route('/printed').get(printedWear)
+route.route('/printed').get(printedWear)  
 route.route('/embrodered').get(embroderedWear)
 route.route('/chickkary').get(chickKaryWear)
 route.route('/:id').get(getByIdUnstitchedCloth)
